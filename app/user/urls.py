df42b53bdcb357fb98422c01e79b67879a416c6e
Sergi Orra <sergiorra17@gmail.com>
2020-06-07 19:17:35 +0200

    working on user API paths

---
+++ app/user/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+# The app_name helps to identify which app we are creating the URL
+# when we use the reverse function
+app_name = 'user'
+
+urlpatterns = [
+    path('create/', views.CreateUserView.as_view(), name='create'),
+]
